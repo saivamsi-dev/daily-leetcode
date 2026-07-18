6dd51ba5ced33373a8ce003f97f91247413ce358
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-07-19 00:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.56%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -1,18 +1,20 @@
 class Solution(object):
     def isGood(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        # The expected maximum element 'n' based on the array length
+        # return count(len(nums)-1)
+        # return nums.count(len(nums) - 1) == 2
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         # The expected maximum element 'n' based on the array length
         n = len(nums) - 1
         
-        # 'base[n]' must have a length of at least 2 (i.e., base[1] = [1, 1])
+#         # 'base[n]' must have a length of at least 2 (i.e., base[1] = [1, 1])
         if n < 1:
             return False
             
-        # Construct the expected base[n] array
+#         # Construct the expected base[n] array
         expected = list(range(1, n)) + [n, n]
         
-        # Sort the input array and check if it matches the expected permutation
+#         # Sort the input array and check if it matches the expected permutation
         return sorted(nums) == expected
