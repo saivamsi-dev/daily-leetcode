f7c840938ac1b19fe322ae904289b6f82781f350
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-07-22 00:51:10 +0530

    Time: 124 ms (100.00%) | Memory: 12.6 MB (83.93%) - LeetSync

--- 3356-shortest-uncommon-substring-in-an-array/shortest-uncommon-substring-in-an-array.py
+++ 3356-shortest-uncommon-substring-in-an-array/shortest-uncommon-substring-in-an-array.py
@@ -1,44 +1,84 @@
+# class Solution(object):
+#     def shortestSubstrings(self, arr):
+#         """
+#         :type arr: List[str]
+#         :rtype: List[str]
+#         """
+#         from collections import defaultdict
+
+#         # Step 1: Count in how many distinct strings each substring appears
+#         sub_counts = defaultdict(int)
+        
+#         for s in arr:
+#             # Collect unique substrings for the current string s
+#             seen_in_s = set()
+#             length = len(s)
+#             for i in range(length):
+#                 for j in range(i + 1, length + 1):
+#                     seen_in_s.add(s[i:j])
+            
+#             for sub in seen_in_s:
+#                 sub_counts[sub] += 1
+        
+#         # Step 2: Find the shortest & lexicographically smallest uncommon substring for each word
+#         ans = []
+#         for s in arr:
+#             candidates = []
+#             length = len(s)
+            
+#             # Generate all substrings of s
+#             for i in range(length):
+#                 for j in range(i + 1, length + 1):
+#                     sub = s[i:j]
+#                     # If substring appears ONLY in string s
+#                     if sub_counts[sub] == 1:
+#                         candidates.append(sub)
+            
+#             if not candidates:
+#                 ans.append("")
+#             else:
+#                 # Sort by length first, then lexicographically
+#                 candidates.sort(key=lambda x: (len(x), x))
+#                 ans.append(candidates[0])
+                
+#         return ans
+
+
 class Solution(object):
     def shortestSubstrings(self, arr):
         """
         :type arr: List[str]
         :rtype: List[str]
         """
-        from collections import defaultdict
-
-        # Step 1: Count in how many distinct strings each substring appears
-        sub_counts = defaultdict(int)
-        
-        for s in arr:
-            # Collect unique substrings for the current string s
-            seen_in_s = set()
-            length = len(s)
-            for i in range(length):
-                for j in range(i + 1, length + 1):
-                    seen_in_s.add(s[i:j])
-            
-            for sub in seen_in_s:
-                sub_counts[sub] += 1
-        
-        # Step 2: Find the shortest & lexicographically smallest uncommon substring for each word
         ans = []
-        for s in arr:
-            candidates = []
-            length = len(s)
-            
-            # Generate all substrings of s
-            for i in range(length):
-                for j in range(i + 1, length + 1):
-                    sub = s[i:j]
-                    # If substring appears ONLY in string s
-                    if sub_counts[sub] == 1:
-                        candidates.append(sub)
-            
-            if not candidates:
-                ans.append("")
-            else:
-                # Sort by length first, then lexicographically
-                candidates.sort(key=lambda x: (len(x), x))
-                ans.append(candidates[0])
-                
+
+        for i in range(len(arr)):
+            s = arr[i]
+            best = ""
+
+            for length in range(1, len(s) + 1):
+                candidates = set()
+
+                # generate substrings of this length
+                for start in range(len(s) - length + 1):
+                    candidates.add(s[start:start + length])
+
+                # check lexicographically smallest first
+                for sub in sorted(candidates):
+                    found_in_other = False
+
+                    for j in range(len(arr)):
+                        if i != j and sub in arr[j]:
+                            found_in_other = True
+                            break
+
+                    if not found_in_other:
+                        best = sub
+                        break
+
+                if best != "":
+                    break
+
+            ans.append(best)
+
         return ans
