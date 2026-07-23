88036bbefe58f3982a684010dfd5c7ac0bdd8531
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-07-23 22:35:20 +0530

    Time: 767 ms (29.95%) | Memory: 22.9 MB (29.47%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.py
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.py
@@ -1,3 +1,37 @@
+# class Solution:
+#     def getBiggestThree(self, grid: List[List[int]]) -> List[int]:
+#         m, n = len(grid), len(grid[0])
+#         sums = set()
+
+#         for r in range(m):
+#             for c in range(n):
+#                 # Radius k = 0 (single cell)
+#                 sums.add(grid[r][c])
+                
+#                 # Try all possible radii k > 0
+#                 k = 1
+#                 while r - k >= 0 and r + k < m and c - k >= 0 and c + k < n:
+#                     current_sum = 0
+                    
+#                     # Top to Right
+#                     for i in range(k):
+#                         current_sum += grid[r - k + i][c + i]
+#                     # Right to Bottom
+#                     for i in range(k):
+#                         current_sum += grid[r + i][c + k - i]
+#                     # Bottom to Left
+#                     for i in range(k):
+#                         current_sum += grid[r + k - i][c - i]
+#                     # Left to Top
+#                     for i in range(k):
+#                         current_sum += grid[r - i][c - k + i]
+                    
+#                     sums.add(current_sum)
+#                     k += 1
+
+#         return sorted(sums, reverse=True)[:3]
+
+
 class Solution:
     def getBiggestThree(self, grid: List[List[int]]) -> List[int]:
         m, n = len(grid), len(grid[0])
@@ -5,10 +39,10 @@ class Solution:
 
         for r in range(m):
             for c in range(n):
-                # Radius k = 0 (single cell)
+                # Size k = 0 (single cell)
                 sums.add(grid[r][c])
                 
-                # Try all possible radii k > 0
+                # Expand radius k > 0
                 k = 1
                 while r - k >= 0 and r + k < m and c - k >= 0 and c + k < n:
                     current_sum = 0
