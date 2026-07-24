ee3fd5eaeb0bb80af918d8bc6b79079ce393089d
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-07-24 22:13:26 +0530

    Time: 11 ms (9.04%) | Memory: 19.2 MB (93.67%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.py
+++ 2159-two-out-of-three/two-out-of-three.py
@@ -1,6 +1,20 @@
+# class Solution:
+#     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+#         s1, s2, s3 = set(nums1), set(nums2), set(nums3)
+        
+#         # Combine pairs present in at least two sets
+#         return list((s1 & s2) | (s2 & s3) | (s1 & s3))
+
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
-        s1, s2, s3 = set(nums1), set(nums2), set(nums3)
+        setnums = set(nums1+nums2+nums3)
+        ans = []
+        for i in setnums:
+            if ((i in nums1 and i in nums2) or (i in nums1 and i in nums3) or (i in nums2 and i in nums3)):
+                ans.append(i)
+        return ans;
         
-        # Combine pairs present in at least two sets
-        return list((s1 & s2) | (s2 & s3) | (s1 & s3))
+
+
+        
+        
