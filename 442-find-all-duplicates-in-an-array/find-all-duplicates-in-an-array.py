5728b52c0265ab6b83e1b8e92d29793592bfec06
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-07-31 04:52:58 +0530

    Time: 84 ms (9.15%) | Memory: 19.7 MB (57.57%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        duplicates = []
-        
-        for num in nums:
-            idx = abs(num) - 1
-            
-            # If the number at index `idx` is already negative, `num` is a duplicate
-            if nums[idx] < 0:
-                duplicates.append(abs(num))
+        i = 0
+        while i < len(nums):
+            correct_idx = nums[i] - 1
+            # Swap if the element is not at its correct position
+            if nums[i] != nums[correct_idx]:
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
             else:
-                # Mark as visited by negating
-                nums[idx] = -nums[idx]
+                i += 1
                 
-        return duplicates
+        # Collect elements that are not in their correct positions
+        return [nums[i] for i in range(len(nums)) if nums[i] != i + 1]
