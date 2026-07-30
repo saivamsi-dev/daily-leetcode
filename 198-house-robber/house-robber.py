6ab9314040763a13491476387a9bb1777b3c746c
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-07-31 04:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (20.78%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        rob1, rob2 = 0, 0
+        
+        # [rob1, rob2, num, num + 1, ...]
+        for num in nums:
+            temp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+            
+        return rob2
