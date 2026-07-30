aa7dd58e0a4728e80c22a235bf7ef5f4228450de
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-07-31 04:48:12 +0530

    Time: 4 ms (23.72%) | Memory: 12.5 MB (0.55%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        total_score = 0
+        for i in range(len(s) - 1):
+            total_score += abs(ord(s[i]) - ord(s[i + 1]))
+        return total_score
