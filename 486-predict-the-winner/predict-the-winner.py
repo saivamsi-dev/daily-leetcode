639985e57af5d75f3754e0a3d8b124d90ee80081
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-08-02 00:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (98.63%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,13 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        # dp[i] will represent the max score difference for subarray starting at index i
+        dp = list(nums)
+
+        # Build DP from smaller subarrays to larger ones
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i] = max(nums[i] - dp[i + 1], nums[j] - dp[i])
+
+        return dp[0] >= 0
