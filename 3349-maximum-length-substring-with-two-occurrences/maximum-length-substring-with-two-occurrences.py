30b4ab9c06d850c9d05be9faaec4d8d3115447a6
saivamsi-dev <saivamsi.dev.1020@gmail.com>
2026-08-15 01:27:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.31%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -20,7 +20,7 @@ class Solution:
                 k += 1  # Increment k as you described!
             
             # Update maximum length found so far
-            current_length = c - k + 1
-            max_len = max(max_len, current_length)
+            # current_length = c - k + 1
+            max_len = max(max_len, c - k + 1)
             
         return max_len
